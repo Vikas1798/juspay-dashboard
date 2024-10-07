857e6e498dae404921f8ed9efe9de646ea23cde5
Vikas1798 <hkvikas1798@gmail.com>
2024-10-08 02:48:51 +0530

    package issue fixed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -26,18 +26,18 @@ export default function Routes(props) {
         },
     ]);
     return (
-        <Suspense fallback={<FallbackLoading />}>
-            <section className='scroll-smooth grid grid-cols-12 items-start dark:bg-[#1C1C1C]'>
-                <LeftDrawer />
-                <div className='col-span-12 xl:col-span-8 border-x-[1px] border-x-[#1C1C1C1A] dark:border-x-[#FFFFFF33]'>
-                    <Header />
-                    <div className='p-4'>
-                        {routes}
-                    </div>
+        // <Suspense fallback={<FallbackLoading />}>
+        <section className='scroll-smooth grid grid-cols-12 items-start dark:bg-[#1C1C1C]'>
+            <LeftDrawer />
+            <div className='col-span-12 xl:col-span-8 border-x-[1px] border-x-[#1C1C1C1A] dark:border-x-[#FFFFFF33]'>
+                <Header />
+                <div className='p-4'>
+                    {routes}
                 </div>
-                <RightDrawer />
-            </section>
-        </Suspense>
+            </div>
+            <RightDrawer />
+        </section>
+        // </Suspense>
 
     );
 }
